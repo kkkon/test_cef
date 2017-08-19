430484d61c530ea3b7e9be2c7711d0ba185e46d6
kkkon <kiyofumi.kondoh+github@gmail.com>
2017-08-19 21:44:40 +0900

    add test resource of custom scheme

--- test_cef_app/cef_app_browser.cpp
+++ test_cef_app/cef_app_browser.cpp
@@ -50,7 +50,8 @@ AppBrowser::OnRegisterCustomSchemes(
 #include "cef_browser_util.h"
 
 namespace {
-const char kStartupURL[] = "https://www.google.com";
+//const char kStartupURL[] = "https://www.google.com";
+const char kStartupURL[] = "file:///resources_scheme/test.html";
 } // namespace {
 
 void
